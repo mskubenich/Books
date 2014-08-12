d7ee59bf20dfe3e64082f74af15a4961105bfacc
misha <misha.skubenich@gmail.com>
2014-08-12 17:18:09 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BooksApp::Application.config.secret_key_base = '1f52781a5942f5e1b14cafced30826911554984737154d1f7e3f3173754ec4fab3b2f63a30abdf2bb1840c9cb5f60340e45b110906a239e15ee4ca24bf397d2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BooksApp::Application.config.session_store :cookie_store, key: '_books_app_session'
