862d47dc68a2af09c98ba3e5594b3e72d3de1203
VasylRishko <rishko92@gmail.com>
2014-08-23 23:48:14 +0300

    Finished task "add avatar to user"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
+
   validates :name,  presence: true, length: { maximum: 50 }
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
   					format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password 
-  validates :password, length: { minimum: 6 }                   
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
+
+  has_attached_file :avatar, :styles => {small: "150x150>"}
+  validates_attachment :avatar, :presence => true, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] } ,
+                       :size => {:in => 0..1.megabytes}
+
 end
