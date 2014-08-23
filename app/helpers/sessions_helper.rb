acb040d2b913c40b4aa51e5a711902b341666e68
GRuslan <garagonuch@mail.ru>
2014-08-23 18:15:23 +0300

    Create Sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,8 +7,8 @@ module SessionsHelper
 	end
 
 	def signed_in?
-		!current_user.nill
-	end
+    !current_user.nil?
+    end
 
 	def current_user=(user)
         @current_user = user
@@ -17,4 +17,11 @@ module SessionsHelper
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
     end
+    
+    def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.encrypt(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
 end
