1ab8251cd02fc716fa6fe4a92db55b67cdb9b238
VasylRishko <rishko92@gmail.com>
2014-08-25 22:14:50 +0300

    fixed user_controller.rb (def create)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create( user_params )
+    @user = User.new( user_params )
     if @user.save
       redirect_to @user
     else
