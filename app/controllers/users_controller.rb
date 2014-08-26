ad7ef693e9fb0a7409cb819673745f5103491206
rchaban <roman18888@gmail.com>
2014-08-26 18:33:44 +0300

    Updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    current_user = User.first # TODO remove after implement login
+    @user = current_user
+  end 
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end 
+
   private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 BooksApp::Application.routes.draw do
-  resources :users
+  resources :users, only: [ :update, :create, :show ]
   get "admins/index", to: "admin/admins#index"
   root :to => "pages#index"
   get "pages/index"
   get "/signup", to: "users#new"
-
+  get "/edit_profile", to: "users#edit"
 end
