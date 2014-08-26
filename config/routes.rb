4fb310a96e87ea612f42be51633f6ac40186a84a
GRuslan <garagonuch@mail.ru>
2014-08-26 17:41:38 +0300

    Add route get pages/index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ BooksApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
+  get '/signup', to: 'users#new'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   get "admins/index", to: "admin/admins#index"
   root :to => "pages#index"
