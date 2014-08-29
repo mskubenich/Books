c44890ef136a516ad2a92201c28220104d2f70c3
VasylRishko <rishko92@gmail.com>
2014-08-29 17:23:22 +0300

    create functional tests for login/logout

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -3,35 +3,37 @@ require_relative '../minitest_helper'
 class UsersControllerTest
   describe UsersController do
 
-	it "should get new" do
-	  get :new
-	  response_must_be :ok
-	end
-
-	it "should post create" do
-	  post :create, {
-	  	   user: 
-	  	   { name: 'Example', email: 'user@gmail.com', password: '[FILTERED]', password_confirmation: '[FILTERED]'
-	  	   }
-	  }
-	  response_must_be :ok
-	end 
-
-	it 'should  get edit' do  
-      @user = FactoryGirl.create(:user)
-      get :edit, id: @user.id
+    it "should get new" do
+      get :new
       response_must_be :ok
-	end
+    end
 
-	it "should post update" do
-	  @user = FactoryGirl.create(:user)
-	  patch :update, {
-	  	    user: 
-	  	    { name: 'Example User', email: 'user@gmail.com', password: '[FILTERED]', password_confirmation: '[FILTERED]'
-	  	    },
-	  	    id: @user.id
-	  }
-	  response_must_be :ok
-	end 	
+    it "should post create" do
+      post :create, {user: { name: 'Example',
+                             email: 'user@gmail.com',
+                             password: '[FILTERED]',
+                             password_confirmation: '[FILTERED]'}}
+      response_must_be :ok
+    end
+
+    it 'should  get edit' do
+        @user = FactoryGirl.create(:user)
+        get :edit, id: @user.id
+        response_must_be :ok
+    end
+
+    it "should post update" do
+      @user = FactoryGirl.create(:user)
+
+      patch :update, {user: { name: 'Example User',
+                              email: 'user@gmail.com',
+                              password: '[FILTERED]',
+                              password_confirmation: '[FILTERED]'},
+
+                      id: @user.id
+                     }
+
+      response_must_be :ok
+    end
   end
 end
