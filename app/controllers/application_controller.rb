cbd1ea6d707058425c93e4eec00bc66d3c32d2c2
misha <misha.skubenich@gmail.com>
2014-09-01 09:35:58 +0300

    Create model Visit. Create visit for each transaction.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :create_visit
+
+  private
+
+  def create_visit
+    ua = AgentOrange::UserAgent.new(request.user_agent)
+    browser = ua.device.engine.browser
+    os = ua.device.operating_system
+
+    Visit.create ip: request.remote_ip,
+                 browser: browser.name,
+                 browser_version: browser.version.to_s,
+                 os_name: os.name,
+                 os_version: os.version.to_s,
+                 device: ua.device.to_s
+  end
 end
---
+++ db/migrate/20140901061454_create_visits.rb
@@ -0,0 +1,13 @@
+class CreateVisits < ActiveRecord::Migration
+  def change
+    create_table :visits do |t|
+      t.string :ip
+      t.string :browser
+      t.string :browser_version
+      t.string :os_name
+      t.string :os_version
+      t.string :device
+      t.timestamps
+    end
+  end
+end
