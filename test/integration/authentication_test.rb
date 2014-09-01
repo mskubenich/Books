83d45077622214fdf44d75dff3ebd82e7b36eb6d
VasylRishko <rishko92@gmail.com>
2014-09-01 19:21:59 +0300

    finish integration test for login/logout

---
+++ test/integration/authentication_test.rb
@@ -0,0 +1,33 @@
+require_relative '../minitest_helper'
+
+class AuthenticationTest
+  describe "Login/Logout Integration", :js => true do
+
+    it 'signin page' do
+      visit signin_path
+
+      page.must_have_content 'Sign in'
+      page.must_have_content 'Sign in'
+    end
+
+    describe "Authentication", :js => true do
+
+      before do
+        @user = FactoryGirl.create(:user, name: "Test User", email: "test@example.com", password: "foobar", password_confirmation: "foobar")
+      end
+
+        it 'signin with valid information' do
+          visit signin_path
+          fill_in "Email", :with => @user.email
+          fill_in "Password", :with => @user.password
+          find('.btn.btn-large.btn-primary').click
+
+          page.must_have_title  @user.name
+          page.must_have_content  @user.name
+          page.must_have_link  'Profile', href: profile_path
+          page.must_have_link  'Sign out', href: signout_path
+          page.must_have_link  'Edit your profile', href: edit_profile_path
+      end
+    end
+  end
+end
