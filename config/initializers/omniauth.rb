cf39f2d60c51496005ece7071ea57639228c9dcd
VasylRishko <rishko92@gmail.com>
2014-09-04 18:37:53 +0300

    edit omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '894150267279528', '202be0a5aa35be986819737b6f677256'
+  provider :twitter, 'AwDEAgrzywx7BYSou6kWbO9aC', 'FPctBjILgQXeDoQe986keCxzsHc5Qlswp4qC5UKk4phRaAsJKR'
 end
