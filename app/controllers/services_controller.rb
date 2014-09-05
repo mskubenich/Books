2c41bb67458383a69ad70d4e1268b84a2e0cd030
VasylRishko <rishko92@gmail.com>
2014-09-05 11:04:54 +0300

    add new service_route 'github'; finish the login with other services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,9 +6,12 @@ class ServicesController < ApplicationController
   end
 
   def create
+    #               Test the callback cache
+    #
     # render :text => request.env["omniauth.auth"].to_yaml
-    omniauth = request.env['omniauth.auth']
+    # render :text => @uid.to_s + " - " + @name.to_s + " - " + @email.to_s + " - " + @provider.to_s
 
+    omniauth = request.env['omniauth.auth']
     if omniauth && params[:service]
       service_route = params[:service]
       @service = service_route
@@ -27,16 +30,20 @@ class ServicesController < ApplicationController
           omniauth['info']['email'] ?  @email =  omniauth['info']['email'] : @email = ''
           omniauth['uid'] ?  @uid =  omniauth['uid'] : @uid = ''
           omniauth['provider'] ? @provider =  omniauth['provider'] : @provider = ''
+        elsif service_route = 'github'
+          omniauth['info']['email'] ? @email =  omniauth['info']['email'] : @email = ''
+          omniauth['info']['nickname'] ? @name =  omniauth['info']['nickname'] : @name = ''
+          omniauth['uid'] ?  @uid =  omniauth['uid'] : @uid = ''
+          omniauth['provider'] ? @provider =  omniauth['provider'] : @provider = ''
         end
-      else render :text => omniauth.to_yaml
+    else render :text => omniauth.to_yaml
     end
-    # render :text => @uid.to_s + " - " + @name + " - " + @email + " - " + @provider
     if @uid != '' and @provider != ''
       auth = Service.find_by_provider_and_uid(@provider, @uid)
         if !auth
           @email = "example@gmail.com" if @email.blank?
           @user = User.create(name: @name, email: @email, password: "foobar", password_confirmation: "foobar",)
-          @user.reload.services.create(:provider => @provider, :uid => @uid, :uname => @name, :uemail => @email)
+            @user.reload.services.create(:provider => @provider, :uid => @uid, :uname => @name, :uemail => @email)
           flash[:notice] = 'Sign in via ' + @provider.capitalize + ' has been added to your account.'
         else
           @user = auth.user
