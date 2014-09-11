463912986f6893a84f7695e56c094e27431dd9dd
VasylRishko <rishko92@gmail.com>
2014-09-11 21:19:05 +0300

    delete .divider

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901061454) do
+ActiveRecord::Schema.define(version: 20140903123812) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 20140901061454) do
 
   create_table "genres", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "services", force: true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "uname"
+    t.string   "uemail"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
