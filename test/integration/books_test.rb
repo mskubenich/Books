459ec80bc934fa49db94776740f170b80dd2e6d8
vitaly <ololo074@gmail.com>
2014-09-22 17:46:01 +0300

    Display books list. Fix broken tests. Integration test.

--- test/integration/books_test.rb
+++ test/integration/books_test.rb
@@ -1,15 +1,29 @@
 require_relative '../minitest_helper'
 
-class BooksTest
+class BooksTest < AcceptanceSpec
   describe "Books", js: true do
+  	before do
+  		@user = FactoryGirl.create :user
+  		@book1 = FactoryGirl.create :book, title: 'dsf'
+  		@book2 = FactoryGirl.create :book, title: 'Kobzar', description: 'Taras Shevchenko', year: '1840', language: 'Ukrainian', pages: '0'
+  	end
 	  it 'shoud show the BookList table' do
 	  	visit '/signin'
 	  	fill_in "Email", :with => @user.email
       fill_in "Password", :with => @user.password
       find('.btn.btn-primary.btn-lg').click
 
-      click_on "BOOKS"
-      dfkaplmd
+      click_on "Books"
+      find('.book-list thead tr:nth-child(1) th:nth-child(1)').must_have_content 'Title'
+      find('.book-list thead tr:nth-child(1) th:nth-child(2)').must_have_content 'Description'
+      find('.book-list tbody tr:nth-child(1) td:nth-child(3)').must_have_content '2013'
+      find('.book-list tbody tr:nth-child(1) td:nth-child(4)').must_have_content 'test language'
+      find('.book-list tbody tr:nth-child(1) td:nth-child(5)').must_have_content '207'
+      find('.book-list tbody tr:nth-child(2) td:nth-child(1)').must_have_content 'Kobzar'
+      find('.book-list tbody tr:nth-child(2) td:nth-child(2)').must_have_content 'Taras Shevchenko'
+      find('.book-list tbody tr:nth-child(2) td:nth-child(3)').must_have_content '1840'
+      find('.book-list tbody tr:nth-child(2) td:nth-child(4)').must_have_content 'Ukrainian'
+      find('.book-list tbody tr:nth-child(2) td:nth-child(5)').must_have_content '0'
 	  end
   end
 end
