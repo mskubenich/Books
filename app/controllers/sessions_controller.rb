428d28b97c2b857765031d184edca353af0c9244
rchaban <roman18888@gmail.com>
2014-09-30 13:02:59 +0300

    Unchanged improve integration tests for login, logout, login with wrong parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
     else
-      flash.now[:error] = 'Invalid email/password combination' unless user
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
