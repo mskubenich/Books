426ffa2d588beb2b7c8c0a4800cdb6da9c1de882
rchaban <roman18888@gmail.com>
2014-10-09 00:56:43 +0300

    Device statistics

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,10 +27,11 @@ puts 'Create visits ...'
 browsers = ['Chrome', 'Opera', 'IE']
 os_names = ['Linux', 'Windows']
 os_versions = ['i686', '64 bit']
+devices = ['Computer', 'Android', 'IOS', 'Windows Phone']
 ip_address = "#{rand(255)}.#{rand(255)}.#{rand(255)}.#{rand(255)}"
 browser_versions = "#{rand(99)}.#{rand(10)}.#{rand(3000)}.#{rand(99)}"
 
 100.times do |i|
-  Visit.create(ip: ip_address, browser: browsers[rand(2)], browser_version: browser_versions, os_name: os_names[rand(2)], os_version: os_versions[rand(2)], device: "Computer",
+  Visit.create(ip: ip_address, browser: browsers[rand(2)], browser_version: browser_versions, os_name: os_names[rand(2)], os_version: os_versions[rand(2)], device: devices[rand(5)],
   	          created_at: Time.now - rand(100).days)
 end
