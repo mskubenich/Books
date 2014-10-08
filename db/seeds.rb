f4ac9c6a5a685fc012b043c112aa0b1fc110c834
rchaban <roman18888@gmail.com>
2014-10-09 00:38:51 +0300

    Chart the type of operating system

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,12 +25,12 @@ Book.create(title: 'The Kobzar', description: 'Taras Shevchenko', year: 1840, la
 
 puts 'Create visits ...'
 browsers = ['Chrome', 'Opera', 'IE']
-os_names = ['Linux', 'Windows']
+os_names = ['Linux', 'Windows 8', 'Mac OS X', 'Windows XP', 'Windows Seven']
 os_versions = ['i686', '64 bit']
 ip_address = "#{rand(255)}.#{rand(255)}.#{rand(255)}.#{rand(255)}"
 browser_versions = "#{rand(99)}.#{rand(10)}.#{rand(3000)}.#{rand(99)}"
 
 100.times do |i|
-  Visit.create(ip: ip_address, browser: browsers[rand(2)], browser_version: browser_versions, os_name: os_names[rand(2)], os_version: os_versions[rand(2)], device: "Computer",
+  Visit.create(ip: ip_address, browser: browsers[rand(2)], browser_version: browser_versions, os_name: os_names[rand(5)], os_version: os_versions[rand(2)], device: "Computer",
   	          created_at: Time.now - rand(100).days)
 end
