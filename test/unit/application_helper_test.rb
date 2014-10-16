14c1764dcd4f3b9ccd8384d56aae3cb681053c4f
vitaly <ololo074@gmail.com>
2014-10-16 09:36:45 +0300

    Fixed

--- test/unit/application_helper_test.rb
+++ test/unit/application_helper_test.rb
@@ -1,15 +1,14 @@
 require_relative '../minitest_helper'
+include ApplicationHelper
 
 class ApplicationHelperTest < AcceptanceSpec
 
 	describe "ApplicationHelper", js: true  do
 		it "should show titles"	do
 			
-			visit signin_path
-			title.must_have_content "BooksApp | Sign in"
-
-			visit signup_path
-			title.must_have_content "BooksApp | Sign Up"
+			@full_title.wont_equal []
+			
+			assert_equal(full_title("Sign up"), "BooksApp | Sign up")
 		end
 	end
 end
